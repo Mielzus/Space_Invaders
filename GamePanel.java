dfa24a37d0b320f5f172ffc874ce3fc8c8538b82
Jason Cleveland <jasonncleveland@gmail.com>
2016-05-18 22:46:23 -0300

    Added alien movement

--- GamePanel.java
+++ GamePanel.java
@@ -1,7 +1,8 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements ActionListener{
 
 	private ImageIcon smallAlien_1 = new ImageIcon("images/aliens/smallInvader.png");
 	private ImageIcon smallAlien_2 = new ImageIcon("images/aliens/smallInvader_2.png");
@@ -10,11 +11,11 @@ public class GamePanel extends JPanel{
 	private ImageIcon largeAlien_1 = new ImageIcon("images/aliens/largeInvader.png");
 	private ImageIcon largeAlien_2 = new ImageIcon("images/aliens/largeInvader_2.png");
 	private ImageIcon ufo = new ImageIcon("images/aliens/ufo.png");
-	
+	private Timer tm = new Timer(5, this);
 	private Alien[][] aliens = new Alien[5][11];
+	int x, y, xVelocity = -1;
 	
-	public GamePanel(){
-		int x = 48, y = 60;
+	private void resetAliens(){
 		for(int i = 0; i < aliens.length; i++){
 			for(int j = 0; j < aliens[i].length; j++){
 				if(i < 1)
@@ -27,8 +28,15 @@ public class GamePanel extends JPanel{
 				x += 64;
 			}
 			y += 64;
-			x = 48;
+			x -= 704;
 		}
+	}
+	
+	public GamePanel(){
+		tm.start();
+		x = 48;
+		y = 30;
+		resetAliens();
 	}
 	
 	public void paintComponent(Graphics g){
@@ -38,9 +46,24 @@ public class GamePanel extends JPanel{
 		for(int i = 0; i < aliens.length; i++){
 			for(int j = 0; j < aliens[i].length; j++){
 				Alien temp = aliens[i][j];
+				g.setColor(Color.WHITE);
+				g.drawString(Integer.toString(temp.getScore()), temp.getX(), temp.getY());
 				if(temp.getVisible())
 					(temp.getIcon()).paintIcon(this, g, temp.getX(), temp.getY());
 			}
 		}
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if(aliens[0][0].getX() < 0 || aliens[0][10].getX() > 800-64){
+			x -= xVelocity;
+			xVelocity *= -1;
+		}
+		else{
+			x += xVelocity;
+		}
+		y = 30;
+		resetAliens();
+		repaint();
 	}
 }
